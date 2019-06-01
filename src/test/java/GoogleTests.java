5232b4f38a13a15aa681cb9ccd3fe471ea19dd59
lkarczewski <lkarczewski@sigma.ug.edu.pl>
2019-06-01 15:26:26 +0200

    Correct resources place, Chrome: Google search existing site test.

--- src/test/java/GoogleTests.java
+++ src/test/java/GoogleTests.java
@@ -1,6 +1,14 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GoogleTests {
 
@@ -11,5 +19,24 @@ public class GoogleTests {
     public void setUp() {
         driver = BrowserManager.initializeBrowser(driver, "Chrome");
         driver.get("https://www.google.com");
+        wait = new WebDriverWait(driver, 5);
+    }
+
+    @Test
+    public void googleSearchExistingSite() {
+        WebElement webElement = driver.findElement(By.name("q"));
+        webElement.sendKeys("YouTube");
+        webElement.sendKeys(Keys.ENTER);
+
+        wait.until(ExpectedConditions.titleContains("YouTube"));
+        driver.findElement(By.className("LC20lb")).click();
+        wait.until(ExpectedConditions.titleIs("YouTube"));
+
+        assertEquals("YouTube", driver.getTitle());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
     }
 }
