982055cf78c261024802c786ad418c49a4a09eab
lkarczewski <lkarczewski@sigma.ug.edu.pl>
2019-06-02 15:58:42 +0200

    Create PageObject for crud edit.

---
+++ src/test/java/PageObjects/PageObjectCrudEdit.java
@@ -0,0 +1,34 @@
+package PageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PageObjectCrudEdit {
+
+    public WebDriver driver;
+    private WebDriverWait wait;
+
+    public PageObjectCrudEdit(WebDriver driver) {
+        this.driver = driver;
+        driver.get("https://crud-post.herokuapp.com/posts/");
+        wait = new WebDriverWait(driver, 15);
+    }
+
+    public void editPost() throws InterruptedException {
+        WebElement element = driver.findElement(By.xpath("//tr[td[2]//text()[contains(., 'Post testowy')]]/td[6]//a[@class='btn btn-primary btn-xs']"));
+        element.click();
+        Thread.sleep(5000);
+        driver.findElement(By.id("post_title")).sendKeys("Nowy tytuł testowy");
+        driver.findElement(By.name("commit")).click();
+    }
+
+    public void editPostIncorrect() throws InterruptedException {
+        WebElement element = driver.findElement(By.xpath("//tr[td[2]//text()[contains(., 'Post testowy')]]/td[6]//a[@class='btn btn-primary btn-xs']"));
+        element.click();
+        Thread.sleep(5000);
+        driver.findElement(By.id("post_title")).clear();
+        driver.findElement(By.name("commit")).click();
+    }
+}
